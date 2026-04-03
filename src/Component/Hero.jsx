80672e04b4992134a023f81a081bb0a0babf604f
freelancerjunayed junayed <freelancerjunayedjunayed@gmail.com>
2026-04-03 23:50:28 +0600

    changed img

--- src/Component/Hero.jsx
+++ src/Component/Hero.jsx
@@ -3,8 +3,8 @@ import { Link } from "react-router";
 
 const images = [
   "../../src/assets/Gemini_Generated_Image_ggh60kggh60kggh6.png",
-  "../../src/assets/madrasa2.jfif",
-  "../../src/assets/madrasa3.jfif",
+  "../../src/assets/Gemini_Generated_Image_ggh60kggh60kggh6.png",
+  "../../src/assets/Gemini_Generated_Image_ggh60kggh60kggh6.png",
 ];
 
 const Hero = () => {
